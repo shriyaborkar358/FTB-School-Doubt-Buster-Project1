c726c56ee40a98c6ccdfe75e68e561186b554395
sakshishewale <sakshishewale2808@gmail.com>
2024-08-21 00:12:52 +0530

    get api is created for fetching specific user id

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -47,4 +47,28 @@ const getAllStudents = async (req,res) =>{
     }
 
 }
-export {createStudent}
+const getStudentById = async (req,res) =>{
+   try{
+    const StudentId = await Student.findById(req.params.id)
+    if(!StudentId){
+        return res.status(404).json({
+            success:false,
+            message:'student not found',
+            data:null
+        })
+    }
+    res.status(200).json({
+            success:true,
+            message:'student found',
+            data:StudentId
+    })
+   }
+   catch(e){
+    res.status(500).json({
+        success:false,
+        message:'error to fetche the student',
+        data:e.message
+    })
+   }
+}
+export {createStudent,getAllStudents,getStudentById}
