af21fd4d9c2a498c23eebf6f0a787beef4d15605
Sdsurve <sdsurve55555@gmail.com>
2024-08-20 12:12:45 +0530

    add signup login controllers

---
+++ server/controllers/Login.js
@@ -0,0 +1,27 @@
+import User from './../models/User.js'
+import bcrypt from 'bcrypt'
+
+const Login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email })
+        if(!user){
+            return res.status(404).json({ message: 'User not found' })
+        }
+            const isValidPassword = await bcrypt.compare(password, user.password)
+            if (isValidPassword) {
+                // const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY,
+                //     { expiresIn: '1h' })
+                res.json({
+                    success: true,
+                    message: 'Logged in successfully',
+                    token: token
+                })
+            }
+        
+    } catch (err) {
+        console.log(err)
+    }
+
+}
+export default Login
