dad17712d6b604aa4ea4e47189d1db7ced2fcc5b
codewithtarde9370 <teja36491@gmail.com>
2024-08-21 11:13:43 +0530

    login and signup page created

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import Home from './../src/views/Home/Home'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import SignUp from './../src/views/Signup/Signup'
+import Login from './../src/views/Login/Login'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/signup",
+    element: <SignUp/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  // {
+  //   path: "/edit/:id",
+  //   element: <Edit/>
+  // },
+  // {
+  //   path: "/about",
+  //   element: <AboutUs/>
+  // },
+  {
+    path: "*",
+    element: <h1>404 Page not found</h1>
+  }
+])
+root.render(<RouterProvider router={router}/>);
